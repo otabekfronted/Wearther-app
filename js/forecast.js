cf0eeeb82e9c4c9074d1163881cab891434d3b6e
Otabekfrontend <otabekmadaminov1022@gmail.com>
2024-06-11 09:44:56 +0500

    weather app

---
+++ js/forecast.js
@@ -0,0 +1,18 @@
+const KEY = "96b947a45d33d7dc1c49af3203966408";
+
+// get data
+
+const getData = async (city) => {
+    const base = "https://api.openweathermap.org/data/2.5/weather";
+    const query = `?q=${city}&units=metric&appid=${KEY}`;
+    loader(true);
+    const request = await fetch(base + query);
+    console.log(request.status);
+    if (request.status != 200) {
+        loader(false);
+        throw new Error("Bunday shahar yoq!!!");
+    }
+    const data = await request.json();
+    loader(false);
+    return data;
+};
